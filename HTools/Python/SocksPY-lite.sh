4b3be75dbeee96b1b5addbeed340be5b75307108
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2022-09-29 10:37:01 -0500

    UPDATE + 2 Menu de Estabilidad
    añadido modelo antiguo y modelo actual, para verificar estabilidad

--- HTools/Python/SocksPY-lite.sh
+++ HTools/Python/SocksPY-lite.sh
@@ -17,7 +17,6 @@ fun_limpram() {
 	sysctl -w vm.drop_caches=0
 	swapoff -a
 	swapon -a
-apt purge python* -y &> /dev/null
 sudo apt install software-properties-common -y &> /dev/null
 apt install python2 -y &> /dev/null
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1 &> /dev/null
@@ -30,8 +29,7 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1 &> 
 }
 function aguarde() {
 	sleep .1
-
-echo -e "SU VERSION DE UBUNTU ${vercion} ES SUPERIOR A 18.04 "
+	echo -e "SU VERSION DE UBUNTU ${vercion} ES SUPERIOR A 18.04 "
 	helice() {
 		fun_limpram >/dev/null 2>&1 &
 		tput civis
@@ -58,9 +56,10 @@ echo -e "	SU VERSION DE UBUNTU ${vercion} ES INFERIOR O 18.04 "
 	[[ -e /etc/fixpython ]] || { 
 	apt install python -y &>/dev/null
 	apt install python3 -y &>/dev/null
+	touch /etc/fixpython
 	}
 }
-sleep 1s && clear
+clear
 mportas () {
 unset portas
 portas_var=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN")
@@ -207,7 +206,15 @@ msg -bar3
     fi
 [[ -z ${texto_soket} ]] && texto_soket='<span style="color: #ff0000;"><strong><span style="color: #ff9900;">By</span>-<span style="color: #008000;">@ChumoGH</span>-ADM</strong></span>'
 
-    [[ ! -z $porta_bind ]] && conf="-b $porta_bind "|| conf="-p $porta_socket "
+mod1() {
+tput cuu1 && tput dl1
+tput cuu1 && tput dl1
+tput cuu1 && tput dl1
+tput cuu1 && tput dl1
+tput cuu1 && tput dl1
+tput cuu1 && tput dl1
+tput cuu1 && tput dl1
+    [[ ! -z $porta_bind ]] && conf="-b $porta_bind " || conf="-p $porta_socket "
     [[ ! -z $pass_file ]] && conf+="-p $pass_file"
     [[ ! -z $local ]] && conf+="-l $local "
     [[ ! -z $response ]] && conf+="-r $response "
@@ -243,8 +250,317 @@ WantedBy=multi-user.target" > /etc/systemd/system/python.$porta_socket.service
             msg -bar3
         }
     fi
+ }
+ 
+ mod2() {
+ tput cuu1 && tput dl1
+ tput cuu1 && tput dl1
+ tput cuu1 && tput dl1
+ tput cuu1 && tput dl1
+ tput cuu1 && tput dl1
+ tput cuu1 && tput dl1
+ tput cuu1 && tput dl1
+texto="$(echo ${texto_soket} | sed 's/\"//g')"
+[[ ! -z $porta_bind ]] && conf="-b $porta_bind " || conf="$porta_socket "
+    #[[ ! -z $pass_file ]] && conf+="-p $pass_file"
+    #[[ ! -z $local ]] && conf+="-l $local "
+    #[[ ! -z $response ]] && conf+="-r $response "
+    #[[ ! -z $IP ]] && conf+="-i $IP "
+    [[ ! -z $texto_soket ]] && conf+=" '$texto'"
+
+#================================================================
+(
+less << PYTHON  > ${ADM_inst}/PDirect.py
+import socket, threading, thread, select, signal, sys, time, getopt
+# Listen
+LISTENING_ADDR = '0.0.0.0'
+if sys.argv[1:]:
+  LISTENING_PORT = sys.argv[1]
+else:
+  LISTENING_PORT = 80  
+#Pass
+PASS = ''
+# CONST
+BUFLEN = 4096 * 4
+TIMEOUT = 60
+DEFAULT_HOST = '127.0.0.1:$local'
+RESPONSE = 'HTTP/1.1 $response <strong>$texto</strong>\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'
+#RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
+class Server(threading.Thread):
+    def __init__(self, host, port):
+        threading.Thread.__init__(self)
+        self.running = False
+        self.host = host
+        self.port = port
+        self.threads = []
+        self.threadsLock = threading.Lock()
+        self.logLock = threading.Lock()
+    def run(self):
+        self.soc = socket.socket(socket.AF_INET)
+        self.soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.soc.settimeout(2)
+        intport = int(self.port)
+        self.soc.bind((self.host, intport))
+        self.soc.listen(0)
+        self.running = True
+        try:
+            while self.running:
+                try:
+                    c, addr = self.soc.accept()
+                    c.setblocking(1)
+                except socket.timeout:
+                    continue
+                conn = ConnectionHandler(c, self, addr)
+                conn.start()
+                self.addConn(conn)
+        finally:
+            self.running = False
+            self.soc.close()
+    def printLog(self, log):
+        self.logLock.acquire()
+        print log
+        self.logLock.release()
+    def addConn(self, conn):
+        try:
+            self.threadsLock.acquire()
+            if self.running:
+                self.threads.append(conn)
+        finally:
+            self.threadsLock.release()
+    def removeConn(self, conn):
+        try:
+            self.threadsLock.acquire()
+            self.threads.remove(conn)
+        finally:
+            self.threadsLock.release()
+    def close(self):
+        try:
+            self.running = False
+            self.threadsLock.acquire()
+            threads = list(self.threads)
+            for c in threads:
+                c.close()
+        finally:
+            self.threadsLock.release()
+class ConnectionHandler(threading.Thread):
+    def __init__(self, socClient, server, addr):
+        threading.Thread.__init__(self)
+        self.clientClosed = False
+        self.targetClosed = True
+        self.client = socClient
+        self.client_buffer = ''
+        self.server = server
+        self.log = 'Connection: ' + str(addr)
+    def close(self):
+        try:
+            if not self.clientClosed:
+                self.client.shutdown(socket.SHUT_RDWR)
+                self.client.close()
+        except:
+            pass
+        finally:
+            self.clientClosed = True
+        try:
+            if not self.targetClosed:
+                self.target.shutdown(socket.SHUT_RDWR)
+                self.target.close()
+        except:
+            pass
+        finally:
+            self.targetClosed = True
+    def run(self):
+        try:
+            self.client_buffer = self.client.recv(BUFLEN)
+            hostPort = self.findHeader(self.client_buffer, 'X-Real-Host')
+            if hostPort == '':
+                hostPort = DEFAULT_HOST
+            split = self.findHeader(self.client_buffer, 'X-Split')
+            if split != '':
+                self.client.recv(BUFLEN)
+            if hostPort != '':
+                passwd = self.findHeader(self.client_buffer, 'X-Pass')
+				
+                if len(PASS) != 0 and passwd == PASS:
+                    self.method_CONNECT(hostPort)
+                elif len(PASS) != 0 and passwd != PASS:
+                    self.client.send('HTTP/1.1 400 WrongPass!\r\n\r\n')
+                elif hostPort.startswith('127.0.0.1') or hostPort.startswith('localhost'):
+                    self.method_CONNECT(hostPort)
+                else:
+                    self.client.send('HTTP/1.1 403 Forbidden!\r\n\r\n')
+            else:
+                print '- No X-Real-Host!'
+                self.client.send('HTTP/1.1 400 NoXRealHost!\r\n\r\n')
+        except Exception as e:
+            self.log += ' - error: ' + e.strerror
+            self.server.printLog(self.log)
+	    pass
+        finally:
+            self.close()
+            self.server.removeConn(self)
+    def findHeader(self, head, header):
+        aux = head.find(header + ': ')
+        if aux == -1:
+            return ''
+        aux = head.find(':', aux)
+        head = head[aux+2:]
+        aux = head.find('\r\n')
+        if aux == -1:
+            return ''
+        return head[:aux];
+    def connect_target(self, host):
+        i = host.find(':')
+        if i != -1:
+            port = int(host[i+1:])
+            host = host[:i]
+        else:
+            if self.method=='CONNECT':
+                port = $local
+            else:
+                port = sys.argv[1]
+        (soc_family, soc_type, proto, _, address) = socket.getaddrinfo(host, port)[0]
+        self.target = socket.socket(soc_family, soc_type, proto)
+        self.targetClosed = False
+        self.target.connect(address)
+    def method_CONNECT(self, path):
+        self.log += ' - CONNECT ' + path
+        self.connect_target(path)
+        self.client.sendall(RESPONSE)
+        self.client_buffer = ''
+        self.server.printLog(self.log)
+        self.doCONNECT()
+    def doCONNECT(self):
+        socs = [self.client, self.target]
+        count = 0
+        error = False
+        while True:
+            count += 1
+            (recv, _, err) = select.select(socs, [], socs, 3)
+            if err:
+                error = True
+            if recv:
+                for in_ in recv:
+		    try:
+                        data = in_.recv(BUFLEN)
+                        if data:
+			    if in_ is self.target:
+				self.client.send(data)
+                            else:
+                                while data:
+                                    byte = self.target.send(data)
+                                    data = data[byte:]
+                            count = 0
+			else:
+			    break
+		    except:
+                        error = True
+                        break
+            if count == TIMEOUT:
+                error = True
+            if error:
+                break
+def print_usage():
+    print 'Usage: proxy.py -p <port>'
+    print '       proxy.py -b <bindAddr> -p <port>'
+    print '       proxy.py -b 0.0.0.0 -p 80'
+def parse_args(argv):
+    global LISTENING_ADDR
+    global LISTENING_PORT
+    
+    try:
+        opts, args = getopt.getopt(argv,"hb:p:",["bind=","port="])
+    except getopt.GetoptError:
+        print_usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print_usage()
+            sys.exit()
+        elif opt in ("-b", "--bind"):
+            LISTENING_ADDR = arg
+        elif opt in ("-p", "--port"):
+            LISTENING_PORT = int(arg)
+def main(host=LISTENING_ADDR, port=LISTENING_PORT):
+    print "\n:-------PythonProxy-------:\n"
+    print "Listening addr: " + LISTENING_ADDR
+    print "Listening port: " + str(LISTENING_PORT) + "\n"
+    print ":-------------------------:\n"
+    server = Server(LISTENING_ADDR, LISTENING_PORT)
+    server.start()
+    while True:
+        try:
+            time.sleep(2)
+        except KeyboardInterrupt:
+            print 'Stopping...'
+            server.close()
+            break
+#######    parse_args(sys.argv[1:])
+if __name__ == '__main__':
+    main()
+PYTHON
+) > $HOME/proxy.log
+
+msg -bar3
+echo -ne "\033[1;97m Ejcutar python directo despues de un reinicio [s/n]: "
+read start_cron
+msg -bar3
+[[ $start_cron = @(s|S|y|Y) ]] && {
+	crontab -l > /root/cron
+	#echo "@reboot screen -dmS pydic-"$porta_socket" python ${ADM_inst}/PDirect.py ${conf} " >> /root/cron
+	echo "@reboot systemctl restart python.${porta_socket}.service" >> /root/cron
+	crontab /root/cron
+	rm /root/cron
+}
+chmod +x ${ADM_inst}/PDirect.py
+#================================================================
+    
+#screen -dmS pydic-"$porta_socket" python ${SCPinst}/PDirect.py "${conf}"
+echo -e "[Unit]
+Description=$1 Service by @ChumoGH
+After=network.target network-online.target nss-lookup.target mysql.service mariadb.service mysqld.service
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+StandardError=journal
+User=root
+WorkingDirectory=/root
+ExecStart=/usr/bin/$py ${ADM_inst}/$1.py $conf
+ExecReload=/bin/kill -HUP $MAINPID
+LimitNOFILE=51200
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/python.$porta_socket.service
+systemctl enable python.$porta_socket &>/dev/null
+systemctl start python.$porta_socket &>/dev/null 
+ }
+
+#-----------SELECCION------------
+selecPython () {
+echo -e "\e[91m\e[43m  ==== SCRIPT MOD ChumoGH|EDICION ====  \033[0m \033[0;33m[$(less ${ADM_inst}/v-local.log)]"
+msg -bar
+echo -ne "$(msg -verd "  [1]") $(msg -verm2 ">") " && msg -azu "Socks WS 1"
+echo -ne "$(msg -verd "  [2]") $(msg -verm2 ">") " && msg -azu "Socks WS 2"
+msg -bar
+echo -ne "$(msg -verd "  [0]") $(msg -verm2 ">") " && msg -bra "   \033[1;41m VOLVER \033[0m"
+msg -bar
+selection=$(selection_fun 2)
+case ${selection} in
+    1)
+    mod1
+    ;;
+    2)
+    mod2
+    ;;
+    0)return 1;;
+esac
+return 1
+}
+#-----------FIN SELECCION--------
+selecPython
     msg -bar
-    print_center -verd "PYTHON INICIADO CON EXITO!!!"
+    [[ $(ps x | grep -w  "PDirect.py" | grep -v "grep" | awk -F "pts" '{print $1}') ]] && print_center -verd "PYTHON INICIADO CON EXITO!!!" || print_center -ama " ERROR AL INICIAR PYTHON!!!"
     msg -bar
     sleep 1
 }
