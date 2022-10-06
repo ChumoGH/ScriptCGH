8df3c8874fef919b3fc9015c6db1a49676d2cf62
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2022-10-06 12:41:08 -0500

    Update SocksPY-lite.sh

--- HTools/Python/SocksPY-lite.sh
+++ HTools/Python/SocksPY-lite.sh
@@ -543,10 +543,10 @@ def parse_args(argv):
 
 def main(host=LISTENING_ADDR, port=LISTENING_PORT):
     
-    print "\033[0;34m━"*8,"\033[1;32m PROXY WEBSOCKET","\033[0;34m━"*8,"\n"
+    print "\033[0;34m━"*8,"\033[1;32m PROXY PYTHON WEBSOCKET","\033[0;34m━"*8,"\n"
     print "\033[1;33mIP:\033[1;32m " + LISTENING_ADDR
     print "\033[1;33mPORTA:\033[1;32m " + str(LISTENING_PORT) + "\n"
-    print "\033[0;34m━"*10,"\033[1;32m VPSMANAGER","\033[0;34m━\033[1;37m"*11,"\n"
+    print "\033[0;34m━"*10,"\033[1;32m ChumoGH ADM - LITE","\033[0;34m━\033[1;37m"*11,"\n"
     
     
     server = Server(LISTENING_ADDR, LISTENING_PORT)
@@ -569,7 +569,7 @@ PYTHON
 msg -bar3
 #systemctl start $py.$porta_socket &>/dev/null
 chmod +x ${ADM_inst}/$1.py
-screen -dmS "ws${porta_socket}" $(which $py) ${ADM_inst}/${1}.py "$conf" & > /root/checkPY.log
+screen -dmS "ws${porta_socket}" $(which $py) ${ADM_inst}/${1}.py "${porta_socket}" & > /root/checkPY.log
 [[ -e $HOME/$1.py ]] && echo -e "\n\n Fichero Alojado en : ${ADM_inst}/$1.py \n\n Respaldo alojado en : $HOME/$1.py \n"
 #================================================================
 if $(ps x | grep $1) ; then
@@ -581,7 +581,7 @@ sleep 1s && tput cuu1 && tput dl1
 #SCREEN -dmS proxy python /etc/SSHPlus/proxy.py 80
 return
 fi
-[[ ! -e /bin/ejecutar/PortPD.log ]] && echo -e "${conf}" > /bin/ejecutar/PortPD.log
+[[ ! -e /bin/ejecutar/PortPD.log ]] && echo -e "${porta_socket}" > /bin/ejecutar/PortPD.log
 }
 
 #-----------SELECCION------------
